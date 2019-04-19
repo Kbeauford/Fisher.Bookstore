991cb30ce046e5acc67ae561684c72b518fc4b79
Kbeauford <beauford.3@osu.edu>
2019-04-19 16:50:14 -0400

    Lab 9 Fix)

--- api/Controllers/BooksController.cs
+++ api/Controllers/BooksController.cs
@@ -2,138 +2,96 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Fisher.Bookstore.Models;
 using Fisher.Bookstore.Data;
+using Fisher.Bookstore.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fisher.Bookstore.Api.Controllers
 {
-
-    [Route("api/book")]
+    [Route("api/books")]
     [ApiController]
-
     public class BooksController : ControllerBase
     {
-        
         private readonly BookstoreContext db;
-/*
-        public BooksController(BookstoreContext db)
+
+        public BooksController(BookstoreContext db) => this.db = db;
+
+        [HttpGet()]
+        public IActionResult GetBooks()
         {
-            this.db= db;
-            if (this.db.Book.Count() == 0)
-            {
-                this.db.Book.Add(new Book()
-                {
-                    Id = 1,
-                    Title = "Design Patterns",
-                    Author = "Erich Gamma",
-                    ISBN = "978-0201633610"
-                });
-                this.db.Book.Add(new Book()
-                {
-                    Id = 2,
-                    Title = "Continuous Delivery",
-                    Author = "Jez Humble",
-                    ISBN = "978-0321601919"
-                });
-                this.db.Book.Add(new Book()
-                {
-                    Id = 3,
-                    Title = "The DevOps Handbook",
-                    Author = "Gene Kim",
-                    ISBN = "978-1942788003"
-                });
-            }
-            this.db.SaveChanges();
-            
-        }
-*/
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return Ok(db.Book);
+            // Use ASP.NET framework for proper status codes
+            // the Ok method returns the model inside the HTTP 200 OK response
+            return Ok(db.Books);
         }
 
         [HttpGet("{id}", Name = "GetBook")]
         public IActionResult GetBook(int id)
         {
-            var book = db.Book.FirstOrDefault(b => b.Id == id);
+            // try to find the correct book 
+            var book = db.Books.FirstOrDefault(b => b.Id == id);
 
+            // if no book is found with the id key, return HTTP 404 Not Found
             if (book == null)
             {
                 return NotFound();
             }
 
+            // return the Book inside HTTP 200 OK
             return Ok(book);
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody]Book book){
-
-            if(book == null){
-
+        [Authorize]
+        public IActionResult CreateBook([FromBody] Book book)
+        {
+            if (book == null)
+            {
                 return BadRequest();
-
             }
 
-            db.Book.Add(book);
+            db.Books.Add(book);
             db.SaveChanges();
-
-            return CreatedAtRoute("GetBook", new { id = book.Id}, book);
-
+            return CreatedAtRoute("GetBook", new { id = book.Id }, book);
         }
 
-
         [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody]Book book){
-
-
-            // validate the incoming book
-            if( book == null || book.Id != id){
-
+        [Authorize]
+        public IActionResult Put(int id, [FromBody] Book book)
+        {
+            if (book == null || book.Id != id)
+            {
                 return BadRequest();
-
             }
 
-            //verify the book is in the database
-            var bookToEdit = db.Book.FirstOrDefault(b => b.Id == id);
-            if(bookToEdit == null){
-
+            var bookToEdit = db.Books.FirstOrDefault(b => b.Id == id);
+            if (bookToEdit == null)
+            {
                 return NotFound();
-
             }
 
             bookToEdit.Title = book.Title;
             bookToEdit.ISBN = book.ISBN;
 
-            db.Book.Update(bookToEdit);
+            db.Books.Update(bookToEdit);
             db.SaveChanges();
-
 
             return NoContent();
         }
-
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id){
-
-            var book = db.Book.FirstOrDefault(b => b.Id == id);
-
-            if(book == null){
-
+        [Authorize]
+        public IActionResult Delete(int id)
+        {
+            var book = db.Books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+            {
                 return NotFound();
-
             }
 
-            db.Book.Remove(book);
+            db.Books.Remove(book);
             db.SaveChanges();
-
             return NoContent();
         }
-
-
     }
-
-
-
 }
