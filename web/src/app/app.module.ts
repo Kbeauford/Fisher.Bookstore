bc745727573de7f9222a2b00850f88d26c58fd23
Kbeauford <beauford.3@osu.edu>
2019-04-19 14:02:33 -0400

    Web All Lab Correctly

---
+++ web/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books/books.component';
+import { BookRowComponent } from './books/book-row/book-row.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BooksComponent,
+    BookRowComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
